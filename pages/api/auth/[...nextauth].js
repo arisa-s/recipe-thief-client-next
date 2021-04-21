952dcca72eb18b9f0bf3552ba27934e2eb95fa25
arisa-s <arisashiraishi@outlook.com>
2021-04-21 12:09:00 -0400

    deploy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -34,11 +34,16 @@ const options = {
   // database: process.env.DATABASE_URL,
   database: {
     type: "mysql",
-    host: "127.0.0.1",
-    port: 3306,
-    username: "root",
-    password: "password",
-    database: "recipe",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    // host: "127.0.0.1",
+    // port: 3306,
+    // username: "root",
+    // password: "password",
+    // database: "recipe",
   },
 };
 
